3d8f56e8c0fc1589aa11690f5184a1b39c3eb6c7
Matthew Faigan <matthew.faigan@gmail.com>
2022-09-16 19:16:10 -0400

    Minor changes to main activity layout

--- app/src/main/java/com/mfaigan/assignment1/MainActivity.java
+++ app/src/main/java/com/mfaigan/assignment1/MainActivity.java
@@ -3,12 +3,18 @@ package com.mfaigan.assignment1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int totalCount = 0; // temporary
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView totalCountDisplay =  findViewById(R.id.textViewTotalCount);
+        totalCountDisplay.setText(String.format("Total Count: %d", totalCount));
     }
 }
